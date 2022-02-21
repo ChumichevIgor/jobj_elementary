29931f05dbcf5d8a5d25b185aeedcbc8864cb96c
Игорь <thisischis@mail.ru>
2022-02-21 22:14:57 +0300

    6.4.1.1. Обход массива с последнего элемента [#361619 #265616]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,12 +2,11 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = true;
-        for (char i = 0; i < word.length / 2; i++) {
+        for (int i = 0; i < pref.length; i++) {
             if (word[i] != pref[i]) {
                 return false;
             }
         }
-        return result;
+        return true;
     }
 }
